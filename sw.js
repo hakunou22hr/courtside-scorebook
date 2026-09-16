7452e06c1569d44c633417b7a364998ba56dff8d
hakunou22hr <hakunou22hr@gmail.com>
2026-09-16 09:35:05 +0900

    Cache complete scorebook app for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='courtside-v2';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest'];
+const CACHE='courtside-v3';
+const ASSETS=['./','index.html','styles.css','app.js','sheet-static.js','manifest.webmanifest','assets/official-scoresheet-template.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
