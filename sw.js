c910086747980166aae2a8656822796ce8a40694
hakunou22hr <hakunou22hr@gmail.com>
2026-09-16 11:43:34 +0900

    Refresh scorebook cache for running-score notation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courtside-v5';
+const CACHE='courtside-v6';
 const ASSETS=['./','index.html','styles.css','app.js','sheet-static.js','scoresheet-field-map.json','manifest.webmanifest','assets/スコアシート.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
